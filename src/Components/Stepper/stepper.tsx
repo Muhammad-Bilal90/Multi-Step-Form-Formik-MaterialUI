f9e049cfa65629745fe772461da75c1febe23708
Muhammad Bilal <bkafridi9833@gmail.com>
2022-08-04 17:27:09 +0500

    solved ui bugs

--- src/Components/Stepper/stepper.tsx
+++ src/Components/Stepper/stepper.tsx
@@ -1,53 +1,93 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import UserInfo from '../UserInfo/userInfo';
-import Security from '../WorkDetails/workDetails';
-import Card from '../CardDetails/cardDetails';
-import { TableBody, TableCell, TableContainer, TableRow, Button } from '@mui/material';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
+import React, { useState } from "react";
+import Container from "@mui/material/Container";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import UserInfo from "../UserInfo/userInfo";
+import WorkDetails from "../WorkDetails/workDetails";
+import CardDetails from "../CardDetails/cardDetails";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+
 
 const getSteps = () => {
-    return ['User Info', 'Work Info', 'Card Info'];
-}
+  return ["User Info", "Work Info", "Card Info"];
+};
 
 const MultiStepper: React.FC = () => {
-
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [user, setUser] = React.useState({firstName: "", lastName: "", email: "", password: "", confirmPassword: ""})
-  const [info, setInfo] = React.useState({officeName: "", designation: ""});
-  const [payment, setPayment] = React.useState({name: "", card: "", cvc: ""});
+  const [activeStep, setActiveStep] = useState(0);
+  const [user, setUser] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [info, setInfo] = useState({ officeName: "", designation: "" });
+  const [payment, setPayment] = useState({ name: "", card: "", cvc: "" });
   const steps = getSteps();
 
   const handleNext = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
-  
+
   const handleBack = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
   const handleReset = () => {
+    setUser({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+    setInfo({ officeName: "", designation: "" });
+    setPayment({ name: "", card: "", cvc: "" });
     setActiveStep(0);
-  }
-
+  };
 
   const getStepsContent = (step: number) => {
-    switch(step){
+    switch (step) {
       case 0:
-        return <UserInfo user={user} setUser={setUser} handleNext={handleNext} handleBack={handleBack} activeStep={activeStep} steps={steps}/>;
+        return (
+          <UserInfo
+            user={user}
+            setUser={setUser}
+            handleNext={handleNext}
+            handleBack={handleBack}
+            activeStep={activeStep}
+            steps={steps}
+          />
+        );
       case 1:
-        return <Security info={info} setInfo={setInfo} activeStep={activeStep} steps={steps} handleNext={handleNext} handleBack={handleBack} />;
+        return (
+          <WorkDetails
+            info={info}
+            setInfo={setInfo}
+            activeStep={activeStep}
+            steps={steps}
+            handleNext={handleNext}
+            handleBack={handleBack}
+          />
+        );
       case 2:
-        return <Card payment={payment} setPayment={setPayment} activeStep={activeStep} steps={steps} handleNext={handleNext} handleBack={handleBack} />;
+        return (
+          <CardDetails
+            payment={payment}
+            setPayment={setPayment}
+            activeStep={activeStep}
+            steps={steps}
+            handleNext={handleNext}
+            handleBack={handleBack}
+          />
+        );
       default:
-        return 'Unknown step';
+        return "Unknown step";
     }
-  }
-  
+  };
+
   return (
     <>
       <Container>
@@ -58,65 +98,54 @@ const MultiStepper: React.FC = () => {
             </Step>
           ))}
         </Stepper>
-        <div>
+        <div className="form">
           {activeStep === steps.length ? (
-            <div>
+            <div className="reviewDetails">
               <h3>Personal Information</h3>
-              <TableContainer component={Paper} style={{ margin: "20px"}}>
-              <Table>
-                <TableBody>
-                  <TableRow>
-                    <TableCell align="right"> <b>First Name:</b></TableCell>
-                    <TableCell>{user.firstName}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell align="right"><b>Last Name:</b></TableCell>
-                    <TableCell>{user.lastName}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell align="right"><b>Email:</b></TableCell>
-                    <TableCell>{user.email}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell align="right"><b>Contact No:</b></TableCell>
-                    <TableCell>{info.officeName}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell align="right"><b>Designation:</b></TableCell>
-                    <TableCell>{info.designation}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell align="right"><b>Card Name:</b></TableCell>
-                    <TableCell>{payment.name}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell align="right"><b>Card No:</b></TableCell>
-                    <TableCell>{payment.card}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell align="right"><b>CVC No:</b></TableCell>
-                    <TableCell>{payment.cvc}</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-              </TableContainer>
-              <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-                  <Button disabled={activeStep === 0} onClick={handleBack}>Back</Button>
-                  <Button variant='contained' color='primary' onClick={handleReset}>
-                      Reset
-                  </Button>
-              </div> 
+              <Paper className="reviewTile">
+                <div style={{ width: "50%" }}>
+                  <p className="reviewTile1">First Name:</p >
+                  <p className="reviewTile1">Last Name:</p >
+                  <p className="reviewTile1">Email:</p >
+                  <p className="reviewTile1">Office Name:</p >
+                  <p className="reviewTile1">Designation:</p >
+                  <p className="reviewTile1">Card Name:</p >
+                  <p className="reviewTile1">Card No:</p >
+                  <p className="reviewTile1">CVC No:</p >
+                </div>
+                <div style={{ width: "50%" }}>
+                  <p className="reviewTile2">{user.firstName}</p>
+                  <p className="reviewTile2">{user.lastName}</p>
+                  <p className="reviewTile2">{user.email}</p>
+                  <p className="reviewTile2">{info.officeName}</p>
+                  <p className="reviewTile2">{info.designation}</p>
+                  <p className="reviewTile2">{payment.name}</p>
+                  <p className="reviewTile2">{payment.card}</p>
+                  <p className="reviewTile2">{payment.cvc}</p>
+                </div>
+              </Paper>
+              <div className="formButtons">
+                <Button disabled={activeStep === 0} onClick={handleBack}>
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleReset}
+                >
+                  Reset
+                </Button>
+              </div>
             </div>
           ) : (
-                <div>
-                  <div>{getStepsContent(activeStep)}</div>
-                </div>
-              )
-          }
+            <div>
+              <div>{getStepsContent(activeStep)}</div>
+            </div>
+          )}
         </div>
       </Container>
     </>
   );
-}
+};
 
 export default MultiStepper;
